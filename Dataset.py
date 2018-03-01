c09a0becda99c9f10447118d9ea8cb7ee3e72a92
Noe <noe.rivals@epitech.eu>
2018-03-01 20:21:44 +0100

    add get_a_ride

--- Dataset.py
+++ Dataset.py
@@ -1,3 +1,5 @@
+from car import *
+from vec import *
 
 class Dataset:
 
@@ -13,3 +15,17 @@ class Dataset:
         self.nb_steps = int(first_line[5])
         for line in range(1, len(lines)):
             self.rides.append(Ride(lines[line]))
+
+    def get_a_ride(self, car, cycle):
+        list_ride = []
+        best_ride = 0
+        for ride in self.rides:
+            if ride.available and ride.is_possible(cycle + car.get_to(ride)):
+                list_ride.append(ride)
+        if len(list_ride) == 0:
+            car.active = False
+        best_ride = list_ride[0]
+        for ride in list_ride:
+            if car.get_distance(ride) < car.get_distance(best_ride):
+                best_ride = ride
+        return ride
--- car.py
+++ car.py
@@ -6,6 +6,7 @@ class car:
     def __init__(self):
         self.pos = Vec(0, 0)
         self.in_ride = False
+        self.active = True
         self.current_ride = 0
         self.rides = []
 
